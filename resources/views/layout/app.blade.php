815d47af41593956c8db9db9a118d037f9174861
Hamza Developer <hamzadeveloper@hotmail.com>
2017-11-14 20:15:35 +0300

    Update

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,10 +6,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="{{ Request::'root' }}/Website/main.css">
+	<link rel="stylesheet" href="{{ asset('css/main.css') }}">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="script.js"></script>
+	<script type="text/javascript" src="{{ asset('js/script.js') }}"></script>
 </head>
 <body>
 
