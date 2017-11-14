10bb054e479365290af2f6e0642609dd46d6f57f
Hamza Developer <hamzadeveloper@hotmail.com>
2017-11-14 20:00:39 +0300

    Update

--- resources/views/page/teachers.blade.php
+++ resources/views/page/teachers.blade.php
@@ -1,1 +1,13 @@
-comment
+@extends('layout.app')
+
+@section('content')
+@if($teachers != null)
+@foreach($teachers as $teacher)
+
+<strong> {{ $teacher->name }} </strong>
+
+@endforeach
+@else
+<strong> Not found any teacher </strong>
+@endif
+@endsection
