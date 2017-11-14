5e70bf573ebc4d156dc27e2862a4e92267411914
Hamza Developer <hamzadeveloper@hotmail.com>
2017-11-14 18:32:27 +0300

    Added Teacher Controller with Get, Post, Put and Delete Methods

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    public function get(Request $req)
+    {
+        
+    }
+
+    public function post(Request $req)
+    {
+        
+    }
+
+    public function put(Request $req)
+    {
+        
+    }
+
+    public function del(Request $req)
+    {
+        
+    }
+}
