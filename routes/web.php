e4a088be365e3418d318d215b45a0517c4422469
iHDeveloper <hamzadeveloper@hotmail.com>
2017-11-15 09:31:57 +0300

    Added Login, Call, ReCall, Done and skip routes also updated all the teacher routes under teacher prefix group and to the admin 2. also added Admin Login Route Page

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,21 @@ Route::get('/dispenser', 'PagesController@dispenser');
 Route::get('/queue', 'PagesController@queue');
 Route::get('/lang/{lang}{route?}', 'LangController@index');
 
+Route::prefix('admin', function(){
+    Route::get('login', 'PagesController@login');
+});
+
 Route::prefix('api/v1', function(){
-    Route::get('/teacher/get/', 'TeacherController@get');
-    Route::post('/teacher/post/{fname?}{mname?}{lname?}{phonenumber?}', 'TeacherController@post');
+    
+    Route::prefix('teacher', function(){
+        Route::get('get/', 'TeacherController@get');
+        Route::post('post/{fname?}{mname?}{lname?}{phonenumber?}', 'TeacherController@post');
+    });
+    Route::prefix('admin', function(){
+        Route::get('login', 'AdminController@login');
+        Route::get('call', 'AdminController@call');
+        Route::get('recall', 'AdminController@recall');
+        Route::get('done', 'AdminController@done');
+        Route::get('skip', 'AdminController@skip');
+    });
 });
