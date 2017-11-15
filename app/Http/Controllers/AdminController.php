634507b253911d8a442be984e25fceb4b4d9bcee
Hamza Developer <hamzadeveloper@hotmail.com>
2017-11-15 16:51:24 +0300

    Update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Webpatser\Uuid\Uuid as UUID;
 use App\AdminPassword;
 use Session;
+use App\Ticket;
 
 class AdminController extends Controller
 {
@@ -34,39 +35,52 @@ class AdminController extends Controller
     }
     
     public function call($id){ // call the next ticket of the teacher
-        $teacher = teacherExist($id); // call the teacher exist with enter the id parm
+        $teacher = $this->teacherExist($id); // call the teacher exist with enter the id parm
         if($teacher == null) return response(404); // if not found the teacher then response 404
-        return; // empty function
+        $now = $teacher->now; // call the now parm in the teacher object
+        if($now != null){ // if it's the first call then it will never add anything on done array
+            $done = json_decode($teacher->done, true); // call the done array and decode it by json
+            array_push($done, $now); // push the now parm of the teacher object into the done array
+            $teacher->done = json_encode($done); // update the done array
+            $ticket = Ticket::find($now); // try to find the ticket of the old one
+            $ticket->destory(); // destory the old ticket
+        }
+        $queue = json_decode($teacher->queue, true); // call the queue array
+        if($queue[0] == null) return response(300); // if there is not anyone in the queue array
+        $now = $queue[0]; // update the now to be the next ticket
+        $teacher->now = $now; // update the now parm in the teacher
+        unset($queue[0]); // remove the now parm from the queue list
+        $teacher->queue = json_encode($queue); // update the queue list in the teacher object
+        $teacher->save(); // save the teacher in the database
+        return response()->json($now); // return the ticket id
     }
     
     public function recall($id){ // re call the ticket of the teacher
-        $teacher = teacherExist($id); // call the teacher exist with enter the id parm
-        if($teacher == null) return response(404); // if not found the teacher then response 404
-        return; // empty function
-    }
-    
-    public function skip($id){ // skip the ticket of the teacher
-        $teacher = teacherExist($id); // call the teacher exist with enter the id parm
-        if($teacher == null) return response(404); // if not found the teacher then response 404
-        return; // empty function
-    }
-    
-    public function done(){ // done the ticket of the teacher
-        $teacher = teacherExist($id); // call the teacher exist with enter the id parm
-        if($teacher == null) return response(404); // if not found the teacher then response 404
-        $now = $teacher->now; // call the now parm in the teacher object
-        $done = json_decode($teacher->done, true); // call the done array and decode it by json
-        array_push($done, $now); // push the now parm of the teacher object into the done array
-        $teacher->done = json_encode($done);
-        // TODO continue the function
-        return; // empty function
-    }
-    
-    public function now(){ // get the ticket of the teacher for now
-        $teacher = teacherExist($id); // call the teacher exist with enter the id parm
+        $teacher = $this->teacherExist($id); // call the teacher exist with enter the id parm
         if($teacher == null) return response(404); // if not found the teacher then response 404
         $now = $teacher->now; // call the now parm in the teacher
         return response()->json($now); // return the now parm from the teacher
+    }
+    
+    public function skip($id){ // skip the ticket of the teacher
+        $teacher = $this->teacherExist($id); // call the teacher exist with enter the id parm
+        if($teacher == null) return response(404); // if not found the teacher then response 404
+        $now = $teacher->now; // call the now parm in the teacher object
+        if($now != null){ // if there is an old call
+            $skip = json_decode($teacher->skip, true); // call the done array and decode it by json
+            array_push($skip, $now); // push the now parm of the teacher object into the done array
+            $teacher->skip = json_encode($skip); // update the skip parm in the teacher
+            $ticket = Ticket::find($now); // find the ticket id by the skip parm
+            $ticket->destory(); // remove the ticket from the database
+        }
+        $queue = json_decode($teacher->queue, true); // call the queue list
+        if($queue[0] == null) return response(300); // if there's not anyone is next to call
+        $now = $queue[0]; // update the now parm by the first item in the queue list
+        $teacher->now = $now; // update the now parm in the teacher
+        unset($queue[0]); // remove the now parm from the queue list
+        $teacher->queue = json_encode($queue); // update the queue list in the teacher object
+        $teacher->save(); // save the teacher object
+        return response()->json($now);
     }
     
     private function teacherExist($id){ // check if the teacher exist or no then return it
