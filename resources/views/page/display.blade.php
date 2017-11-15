54cc7c3036ff8e38dc46590afd9ffd454246a329
AliMd <saudi.gamer.999@gmail.com>
2017-11-15 17:45:47 +0300

    Removed all othere display and leaved the main one

--- resources/views/page/display.blade.php
+++ resources/views/page/display.blade.php
@@ -5,54 +5,16 @@
 
         <!-- Main number that mean the number that come in the middle of the screen it set here until anthore number is pressed - (Ali) -->
 	<!-- Display Screen -->
+	
 	<div class="w3-container" id="display-screen">
-		<div style="float:left; width:20%">
-			<div class="w3-card-4 w3-center" style="padding:20px; margin:20px">
-				<p class="w3-medium">Number:</p>
-				<p class="w3-large">0002</p>
-				<p class="w3-medium">At:</p>
-				<p class="w3-large">COUNTER 1</p>
-			</div>
-			<div class="w3-card-4 w3-center" style="padding:20px; margin:20px">
-				<p class="w3-medium">Number:</p>
-				<p class="w3-large">0003</p>
-				<p class="w3-medium">At:</p>
-				<p class="w3-large">COUNTER 1</p>
-			</div>
-			<div class="w3-card-4 w3-center" style="padding:20px; margin:20px">
-				<p class="w3-medium">Number:</p>
-				<p class="w3-large">0004</p>
-				<p class="w3-medium">At:</p>
-				<p class="w3-large">COUNTER 1</p>
+<center>		<div style="float:center;width:60%">
+<center>		<div class="w3-card-4 w3-center" style="padding:40px; margin:20px; height:700px">
+<center>		<p style="font-size:50px">Number:</p>
+<center>		<p style="font-size:70px">0001</p>
+<center>		<p style="font-size:50px">At:</p>
+<center>		<p style="font-size:70px">COUNTER 1</p>
 			</div>
 		</div>
-		<div style="float:left;width:60%">
-			<div class="w3-card-4 w3-center" style="padding:40px; margin:20px; height:700px">
-				<p style="font-size:50px">Number:</p>
-				<p style="font-size:70px">0001</p>
-				<p style="font-size:50px">At:</p>
-				<p style="font-size:70px">COUNTER 1</p>
-			</div>
-		</div>
-		<div style="float:left; width:20%">
-			<div class="w3-card-4 w3-center" style="padding:20px; margin:20px">
-				<p class="w3-medium">Number:</p>
-				<p class="w3-large">0005</p>
-				<p class="w3-medium">At:</p>
-				<p class="w3-large">COUNTER 1</p>
-			</div>
-			<div class="w3-card-4 w3-center" style="padding:20px; margin:20px">
-				<p class="w3-medium">Number:</p>
-				<p class="w3-large">0006</p>
-				<p class="w3-medium">At:</p>
-				<p class="w3-large">COUNTER 1</p>
-			</div>
-			<div class="w3-card-4 w3-center" style="padding:20px; margin:20px">
-				<p class="w3-medium">Number:</p>
-				<p class="w3-large">0007</p>
-				<p class="w3-medium">At:</p>
-				<p class="w3-large">COUNTER 1</p>
-			</div>
 		</div>
 	</div>
 </div>
